82808f3706b99f6fe13d0cbe1320ddb868824f03
Filatov Denis <demalaron@yandex.ru>
2022-12-21 17:43:36 +0300

    try fix gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="https://denwer81.github.io/react-burger">
+      <BrowserRouter basename="/react-burger">
         <App />
       </BrowserRouter>
     </Provider>
